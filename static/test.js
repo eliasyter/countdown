6a9958b116115f0fa9996fc065c68b74d71bcc72
Elias <elias.yter@gmail.com>
2022-10-08 02:51:43 +0200

    testing compatebility problem with ios

--- static/test.js
+++ static/test.js
@@ -38,5 +38,5 @@ function getTimeRemaining(endtime) {
     const timeinterval = setInterval(updateClock, 1000);
   }
   
-  const deadline = new Date(new Date("5 31, 2023 13:0:0").getTime());
+  const deadline = new Date(new Date("5 31, 2023 13:1:1").getTime());
   initializeClock('clockdiv', deadline);
