b9ce16511b27c1d643f71c934018128d697ed38f
Elias <elias.yter@gmail.com>
2022-10-08 03:04:10 +0200

    fixed the problem with compatebility in ios

--- static/main.js
+++ static/main.js
@@ -1,28 +1,43 @@
-// Set the date we're counting down to
-var countDownDate = new Date("5 31, 2023 13:00:00").getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ".replace(/\s/, 'T');
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "Du er her med meg<4";
+function getTimeRemaining(endtime) {
+    const total = endtime.getTime() - new Date().getTime();
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
   }
-}, 1000);
+  
+  function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.days');
+    const hoursSpan = clock.querySelector('.hours');
+    const minutesSpan = clock.querySelector('.minutes');
+    const secondsSpan = clock.querySelector('.seconds');
+  
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+  
+      daysSpan.innerHTML = (t.days).toString();
+      hoursSpan.innerHTML = (t.hours).toString();
+      minutesSpan.innerHTML = (t.minutes).toString();
+      secondsSpan.innerHTML = (t.seconds).toString();
+  
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }
+  
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+  }
+  
+  var arr = "2023-05-31 13:30:00".split(/[- :]/)
+  const deadline = new Date(arr[0], arr[1]-1, arr[2], arr[3], arr[4], arr[5]);
+  initializeClock('clockdiv', deadline);
